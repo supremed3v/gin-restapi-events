4dd9a09ecef2e22eb307505f86b50ff12127ae22
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2024-12-26 03:35:50 +0500

    configured database and created create table query

--- db/db.go
+++ db/db.go
@@ -2,22 +2,42 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	var err error
-	DB, err = sql.Open("sqlite3", "api.db")
 
+	db, err := sql.Open("sqlite", "api.sql")
 	if err != nil {
-		panic("Could not connect to database.")
+		panic("Database could not connect: " + err.Error())
 	}
 
-	DB.SetMaxOpenConns(10)
-	DB.SetMaxIdleConns(5)
+	DB = db
 
-	createTables()
+	err = createTables()
+	if err != nil {
+		panic("Database could not connect: " + err.Error())
+	}
+
+	fmt.Println("Tables created successfully!")
+}
+
+func createTables() error {
+	createEventsTable := `
+        CREATE TABLE IF NOT EXISTS events (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            description TEXT NOT NULL,
+            location TEXT NOT NULL,
+            dateTime DATETIME NOT NULL,
+            user_id INTEGER
+        )
+    `
+
+	_, err := DB.Exec(createEventsTable)
+	return err
 }
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"net/http"
 
+	"example.com/event-app/db"
 	"example.com/event-app/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
