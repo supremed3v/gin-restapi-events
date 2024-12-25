7726426130cecaf92299b342272ed7d320da548b
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2024-12-26 03:07:19 +0500

    working on API for get and post requests of events

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"example.com/event-app/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
 
 }
 
 func getEvents(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{"message": "working", "status": "ok"})
+	events := models.GetAllEvents()
+	ctx.JSON(200, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+	if err != nil {
+		ctx.JSON(401, err)
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "event created",
+		"event":   event,
+	})
+
 }
