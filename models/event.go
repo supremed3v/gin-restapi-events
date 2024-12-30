a3f57a6bdaf08f3e6362293221a35d58514b4eb8
Saad Siddiqui <saadsiddiqui-programmer@protonmail.com>
2024-12-30 09:00:38 +0500

    completed REST API

--- models/event.go
+++ models/event.go
@@ -121,3 +121,15 @@ func (e Event) Register(userId int64) error {
 
 	return err
 }
+
+func (e Event) CancelRegistration(userId int64) error {
+	query := "DELETE FROM registrations WHERE event_id = ? AND user_id = ?"
+
+	stmt, err := db.DB.Prepare(query)
+
+	_, err = stmt.Exec(e.ID, userId)
+	defer stmt.Close()
+
+	return err
+
+}
--- routes/register.go
+++ routes/register.go
@@ -43,4 +43,32 @@ func registerForEvent(ctx *gin.Context) {
 	})
 
 }
-func cancelRegistration(ctx *gin.Context) {}
+func cancelRegistration(ctx *gin.Context) {
+	userId := ctx.GetInt64("userId")
+
+	eventId, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Couldnt parse event id",
+		})
+		return
+	}
+
+	var event models.Event
+
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Coudln't cancel registration",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Cancelled",
+	})
+}
